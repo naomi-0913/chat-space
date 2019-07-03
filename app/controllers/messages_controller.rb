07bdedbb21a8dffaed4f92335896f44342a2b6d2
naomi-0913 <upworld409019@gmail.com>
2019-07-03 13:10:46 +0900

    setting-imageuploader

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,4 +2,16 @@ class MessagesController < ApplicationController
   def index
   end
 
+  def create
+    if Message.create(message_params)
+      redirect_to group_messages(@group)
+    else
+      render :index
+    end
+  end
+  
+  private
+  def message_params
+    params.require(:message).permit(:cotent, :image)
+  end
 end
