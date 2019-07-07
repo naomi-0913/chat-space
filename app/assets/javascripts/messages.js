70c6c8d0e523df79308fb0dc0d0d41642781b4e2
naomi-0913 <upworld409019@gmail.com>
2019-07-07 12:01:35 +0900

    revise-again

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,19 +1,7 @@
 $(function(){
   function buildMessage(message){
-    console.log(message)
 
-    if (message.content == null ){
-      var message__text = ''
-      }else{
-      var message__text = `<p class>${message.content}</p>`
-      }
-
-    if (message.image.url == null){
-      var message__image = ''
-      }else{
-      var message__image = `<img src="${message.image.url}
-      "></div>`
-      }
+    var message__image = (message.image.url) ?  `<img src="${message.image.url}">` : ''
 
     var html = `<div class="message">
                   <div class="message__upper-info">
@@ -25,8 +13,10 @@ $(function(){
                     </p>
                   </div>
                   <div class="lower-message">
-                  ${message__text}
-                  ${message__image}
+                  <p class = message__text>
+                  ${message.content}
+                  </p>
+                ${message__image}
                   </div>
                 </div>`
     return html;
